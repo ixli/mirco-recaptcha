a73dfc08d4f7f4cd83bc90772b61a4ca33aa2fd1
kaidi.ren <kaidi.ren@upai.com>
2014-05-06 19:04:29 +0800

    update test

--- test/app.js
+++ test/app.js
@@ -1,32 +1,52 @@
 var assert = require("assert"),
+    async = require("async"),
     should = require('should'),
     request = require('supertest');
 
 
-var app = require("../lib/recaptcha.js");
+var app = require("../lib/recaptcha.js"),
+    db = require("../lib/db.js");
 
 
-
-describe('GET /user add', function(){
-  it('respond with json', function(done){
+describe('GET /user add 1', function(){
+  it('user exist!', function(done){
     request(app)
       .get('/user/add?name=rkdrkd')
       .set('Accept', 'application/json')
-      .expect(200, done);
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        done()
+        res.body.should.have.property('status','your name exists ,please try another one!');    
+       })
   })
 })
 
 
 
-describe('GET /user add', function(){
-  it('respond with json', function(done){
-    request(app)
-      .get('/user/add?name=rkdrkd8')
-      .set('Accept', 'application/json')
-      .expect(200, done);
-  })
+
+
+
+
+describe('GET /user add 2', function(){
+    it('respond with json', function(done){
+        db.name_ak_db.get("rkdrkd8", function(err, value){
+            if(typeof(value) != 'undefine'){
+                db.ak_sk_db.del(value, function(){});
+                db.ak_code_db.del(value, function(){});
+            }
+        })
+        db.name_ak_db.del("rkdrkd8",function(){});
+        request(app)
+        .get('/user/add?name=rkdrkd8')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            done()
+            res.body.should.have.property('status','success');    
+        })
+    })
 })
-
-
 
 
